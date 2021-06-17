ef4b48ab1a189f633eac7d428808d81e502eb4ad
Artur Trocki <artur.trocki@gmail.com>
2021-06-17 08:48:45 +0200

    Initial commit

---
+++ src/main/java/pl/atrocki/therooom/TheRooomApplication.java
@@ -0,0 +1,13 @@
+package pl.atrocki.therooom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheRooomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheRooomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/atrocki/therooom/TheRooomApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.atrocki.therooom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheRooomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
